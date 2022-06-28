84eab2d6a9f3429c8cedbb8c8f72f785496c2cb2
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-06-28 20:27:45 +0300

    finish modul 4

--- js/modul_4.js
+++ js/modul_4.js
@@ -1103,3 +1103,111 @@
 //   .map((book) => book.author);
 
 // console.log(names);
+
+/*
+? Дополни функцию getNamesSortedByFriendCount(users) так, чтобы она возвращала массив имён пользователей 
+? отсортированный по возрастанию количества их друзей (свойство friends).
+*/
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+  },
+];
+// Change code below this line
+// const getNamesSortedByFriendCount = [...users]
+//   .sort(
+//     (firstUser, secondUser) =>
+//       firstUser.friends.length - secondUser.friends.length
+//   )
+//   .map((user) => user.name);
+// // Change code above this line
+// console.log(getNamesSortedByFriendCount);
+
+/*
+? Дополни функцию getSortedFriends(users) так, чтобы она возвращала массив уникальных имён друзей (свойство friends) 
+? отсортированный по алфавиту .
+*/
+// const getSortedFriends = (users) =>
+//   [...users]
+//     .flatMap((user) => user.friends)
+//     .sort((firstUser, secondUser) => firstUser.localeCompare(secondUser))
+//     .filter((friend, index, array) => array.indexOf(friend) === index);
+
+// console.log(getSortedFriends(users));
+
+/*
+? Дополни функцию getTotalBalanceByGender(users, gender) так, чтобы она возвращала общий баланс пользователей 
+? (свойство balance), пол которых (свойство gender) совпадает со значением параметра gender.
+*/
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) =>
+  [...users]
+    .filter((user) => user.gender === gender)
+    .reduce((acc, user) => acc + user.balance, 0);
+// Change code above this line
+console.log(getTotalBalanceByGender(users, "male"));
+// // Change code below this line
+// const getTotalBalanceByGender = (users, gender) =>
+//     [...users].filter(t => t.gender === gender).reduce((acc,item) => acc += item.balance, 0)
+//     ;
+// // Change code above this line

4b4e98b403cd904e47e9bf78a02269a260412ffb
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-06-29 15:31:07 +0300

    add exemples

--- js/module_5.js
+++ js/module_5.js
@@ -136,25 +136,25 @@
 ? Измени код, построив цепочку прототипов так, чтобы 
 ? объект ancestor был прототипом для parent, а тот в свою очередь был прототипом для child.
 */
-const ancestor = {
-  name: "Paul",
-  age: 83,
-  surname: "Dawson",
-  heritage: "Irish",
-};
-// Change code below this line
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
 
-const parent = Object.create(ancestor);
-parent.name = "Stacey";
-parent.surname = "Moore";
-parent.age = 54;
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
 
-const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
 
-// Change code above this line
-console.log(child.heritage);
+// // Change code above this line
+// console.log(child.heritage);
 
 /*
 ? Используя ключевое слово class объяви класс Car с пустым телом.

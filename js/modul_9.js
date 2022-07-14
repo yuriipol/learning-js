940de4968146343afbe2823b1e59807d37df49ea
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-14 21:12:41 +0300

    add task for promise

--- js/modul_9.js
+++ js/modul_9.js
@@ -1,26 +1,108 @@
-const date = new Date();
-console.log("Date: ", date);
+/*
+! task 1 PROMISE
+ */
+//? First step
+// console.log("Первый лог обращения!!!");
+// const list = fetch("https://api.sampleapis.com/beers/ale");
+// console.log("Ура все получили!");
+// console.log("Beers -->", list);
+//? Second step
+// const listPromise = fetch("https://api.sampleapis.com/beers/ale");
+// listPromise.then((data) => {
+//   console.log(data);
+// });
+//? Third step
+// const listPromise = fetch("https://api.sampleapis.com/beers/ale");
+// listPromise
+//   .then((data) => data.json())
+//   .then((beers) => {
+//     console.log(beers);
+//   });
+//? Fourth step
+// const listPromise = fetch("https://api.sampleapis.com/beers/ale");
+// listPromise
+//   .then((data) => data.json())
+//   .then((beers) => beers)
+//   .catch((error) => console.log("Ошибка!-->>", error));
+//? CREATE PROMISE STEP1
+// const beers = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject(Error("Ваше пиво подано АЛКАШ!!!"));
+//   }, 2000);
+// });
 
-// Возвращает день месяца от 1 до 31
-console.log("getUTCDate(): ", date.getUTCDate());
+// beers.then((data) => console.log(data)).catch((err) => console.log(err));
+//? Создадим функцию, которая будет передавать id и возвращать обьект одного члена компании,
+//? но будет его менять, расширяя coofee: {все характеристики кофе}
+// const company = [
+//   { name: "Yurii", id: 111, coffee: "Latte" },
+//   { name: "Oleksandr", id: 222, coffee: "Espresso" },
+//   { name: "Maksim", id: 333, coffee: "Cappucino" },
+// ];
+// const getCofee = (name) => {
+//   const coffePromise = fetch("https://api.sampleapis.com/coffee/hot");
+//   return coffePromise
+//     .then((data) => data.json())
+//     .then((list) => {
+//       const coffee = list.find((el) => el.title === name.coffee);
+//       return {
+//         ...name,
+//         coffee,
+//       };
+//     });
+// };
 
-// Возвращает день недели от 0 до 6
-console.log("getUTCDay(): ", date.getUTCDay());
+// const getCompanyMember = (id) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const name = company.find((res) => res.id === id);
+//       if (name) {
+//         resolve(name);
+//       } else {
+//         reject(Error("Член компании не найден!"));
+//       }
+//     }, 2000);
+//   });
+// };
 
-// Возвращает месяц от 0 до 11
-console.log("getUTCMonth(): ", date.getUTCMonth());
+// getCompanyMember(222)
+//   .then((data) => getCofee(data))
+//   .then((newName) => console.log(newName))
+//   .catch((error) => console.log(error));
+//? Если мы ожидаем данные от двух и более промисов
 
-// Возвращает год из 4 цифр
-console.log("getUTCFullYear(): ", date.getUTCFullYear());
+// const makeCoffee = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Coffee complite!");
+//     }, 2000);
+//   });
+// };
 
-// Возвращает час
-console.log("getUTCHours(): ", date.getUTCHours());
+// const makeToast = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Toast complite!");
+//     }, 3000);
+//   });
+// };
 
-// Возвращает минуты
-console.log("getUTCMinutes(): ", date.getUTCMinutes());
+// const coffeePromise = makeCoffee();
+// const toastPromise = makeToast();
 
-// Возвращает секунды
-console.log("getUTCSeconds(): ", date.getUTCSeconds());
+// coffeePromise.then((data) => console.log(data));
+// toastPromise.then((data) => console.log(data));
+// //!или еще так
+// Promise.all([coffeePromise, toastPromise]).then(
+//   ([coffeePromise, toastPromise]) => console.log(coffeePromise, toastPromise)
+// );
+//? 2 промиса через fetch
 
-// Возвращает миллисекунды
-console.log("getUTCMilliseconds(): ", date.getUTCMilliseconds());
+const beersPromise = fetch("https://api.sampleapis.com/beers/ale");
+const winesPromise = fetch("https://api.sampleapis.com/wines/reds");
+
+Promise.all([beersPromise, winesPromise]).then((data) => {
+  return Promise.all(data.map((el) => el.json())).then((finalData) =>
+    console.log(finalData)
+  );
+});

c8a62e6b9ed7b3300e2de7a8bd3ef527058d9fa4
yuriipol <104125040+yuriipol@users.noreply.github.com>
2022-07-19 22:17:53 +0300

    add exemple

--- js/modul_9.js
+++ js/modul_9.js
@@ -116,6 +116,54 @@
 // };
 // console.log(getname(array));
 // Change this number to fetch different post
+
+// const fetchUsers = async () => {
+//   const baseUrl = "https://jsonplaceholder.typicode.com";
+//   const userIds = [1, 2, 3];
+
+//   // 1. Создаём массив промисов
+//   const arrayOfPromises = userIds.map(async (userId) => {
+//     const response = await fetch(`${baseUrl}/users/${userId}`);
+//     return response.json();
+//   });
+
+//   // 2. Запускаем все промисы параллельно и ждем их завершения
+//   const users = await Promise.all(arrayOfPromises);
+//   console.log(users);
+// };
+
+// fetchUsers();
+// const fetchUsers = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const users = await response.json();
+//   return users;
+// };
+
+// const doStuff = async () => {
+//   try {
+//     const users = await fetchUsers();
+//     console.log(users);
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+
+// doStuff();
+
+// const fetchUsers = async () => {
+//   const baseUrl = "https://jsonplaceholder.typicode.com";
+//   const firstResponse = await fetch(`${baseUrl}/users/1`);
+//   const secondResponse = await fetch(`${baseUrl}/users/2`);
+//   const thirdResponse = await fetch(`${baseUrl}/users/3`);
+
+//   const firstUser = await firstResponse.json();
+//   const secondUser = await secondResponse.json();
+//   const thirdUser = await thirdResponse.json();
+
+//   console.log(firstUser, secondUser, thirdUser);
+// };
+
+// fetchUsers();
 const fetchUsers = async () => {
   const baseUrl = "https://jsonplaceholder.typicode.com";
   const userIds = [1, 2, 3];
